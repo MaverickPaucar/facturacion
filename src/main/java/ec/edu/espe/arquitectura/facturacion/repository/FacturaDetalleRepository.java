03d72121f3cba700519737f10ef6c097d94740ae
MaverickPaucar <mrpaucar@espe.edu.ec>
2023-06-14 01:30:04 -0500

    BD Script and Repostories

---
+++ src/main/java/ec/edu/espe/arquitectura/facturacion/repository/FacturaDetalleRepository.java
@@ -0,0 +1,15 @@
+package ec.edu.espe.arquitectura.facturacion.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ec.edu.espe.arquitectura.facturacion.model.FacturaDetalle;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface FacturaDetalleRepository extends JpaRepository<FacturaDetalle, Integer> {
+    List<FacturaDetalle> findByCodigoFactura(Integer codigoFactura);
+
+}
--- src/main/java/ec/edu/espe/arquitectura/facturacion/repository/ProductoRepository.java
+++ src/main/java/ec/edu/espe/arquitectura/facturacion/repository/ProductoRepository.java
@@ -4,11 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import ec.edu.espe.arquitectura.facturacion.model.Producto;
 import java.util.List;
+import java.math.BigDecimal;
 
 public interface ProductoRepository extends JpaRepository<Producto, String> {
 
-    List<Producto> findByNombre(String nombre);
+    List<Producto> findByNombreContainingAndEstado(String nombre, String estado);
 
-    List<Producto> findByNombreLikeOrderByNombre(String nombre);
+    List<Producto> findByEstadoOrderByNombreAsc(String estado);
 
+    Producto findByCodigo(String codigo);
+
+    List<Producto> findByExistenciaLessThanAndEstado(BigDecimal existencia, String estado);
 }
