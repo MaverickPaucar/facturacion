14f3712047784ceed6e80e628d5b92dab8659c5c
MaverickPaucar <mrpaucar@espe.edu.ec>
2023-06-14 01:49:33 -0500

    Models and Repositories

--- src/main/java/ec/edu/espe/arquitectura/facturacion/repository/ImpuestoPorcentajeRepository.java
+++ src/main/java/ec/edu/espe/arquitectura/facturacion/repository/ImpuestoPorcentajeRepository.java
@@ -1,10 +1,16 @@
 package ec.edu.espe.arquitectura.facturacion.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import ec.edu.espe.arquitectura.facturacion.model.Impuesto;
 import ec.edu.espe.arquitectura.facturacion.model.ImpuestoPorcentaje;
 import ec.edu.espe.arquitectura.facturacion.model.ImpuestoPorcentajePK;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImpuestoPorcentajeRepository extends JpaRepository<ImpuestoPorcentaje, ImpuestoPorcentajePK> {
+    List<ImpuestoPorcentaje> findByImpuestoAndEstado(Impuesto impuesto, String estado);
 
 }
--- src/main/java/ec/edu/espe/arquitectura/facturacion/repository/ImpuestoRepository.java
+++ src/main/java/ec/edu/espe/arquitectura/facturacion/repository/ImpuestoRepository.java
@@ -1,9 +1,19 @@
 package ec.edu.espe.arquitectura.facturacion.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ec.edu.espe.arquitectura.facturacion.model.Impuesto;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface ImpuestoRepository extends JpaRepository<Impuesto, String> {
+    List<Impuesto> findByNombre(String nombre);
+
+    List<Impuesto> findByNombreLikeOrderByNombre(String nombre);
+
+    List<Impuesto> findBySiglas(String siglas);
 
 }
